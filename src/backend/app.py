65fdf95ae980a0e36ba832f267a190dff2527587
Jayden Pyles <jpylesbusiness@gmail.com>
2024-04-26 16:15:20 -0500

    chore: remove unused routes

--- src/backend/app.py
+++ src/backend/app.py
@@ -25,13 +25,3 @@ app.add_middleware(
 app.include_router(student_router)
 app.include_router(ChatRouter)
 load_dotenv()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
